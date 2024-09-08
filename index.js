2b27265ff74790d4f0392fd5030e27b8d58836e8
Gideon <48917820+GideonIsBuilding@users.noreply.github.com>
2024-09-08 16:46:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
